5d7ac5ca046c7cceeccb57d0da65f18f0993c8a5
ibnu fadhil <fadhilgelaws@gmail.com>
2024-07-04 16:42:12 +0700

    fixed the sim routes, will be adding produk_socket and produk_tipe_ram soon

--- backend/routes/simulationRoutes.ts
+++ backend/routes/simulationRoutes.ts
@@ -7,29 +7,6 @@ import {
   deleteSocket,
 } from "../controllers/simulation/socketController";
 
-import {
-  getAllDetailRAM,
-  getDetailRAMById,
-  createDetailRAM,
-  updateDetailRAM,
-  deleteDetailRAM,
-} from "../controllers/simulation/detailRAMController";
-
-import {
-  getAllDetailCPU,
-  getDetailCPUById,
-  createDetailCPU,
-  updateDetailCPU,
-  deleteDetailCPU,
-} from "../controllers/simulation/detailCPUController";
-
-import {
-  getAllDetailMotherboard,
-  getDetailMotherboardById,
-  createDetailMotherboard,
-  updateDetailMotherboard,
-  deleteDetailMotherboard,
-} from "../controllers/simulation/detailMotherboardController";
 
 import {
   getAllTipeRAM,
@@ -48,26 +25,6 @@ simulationRoutes.post("/socket", createSocket);
 simulationRoutes.put("/socket/:id", updateSocket);
 simulationRoutes.delete("/socket/:id", deleteSocket);
 
-// Detail_RAM routes
-simulationRoutes.get("/detailram", getAllDetailRAM);
-simulationRoutes.get("/detailram/:id", getDetailRAMById);
-simulationRoutes.post("/detailram", createDetailRAM);
-simulationRoutes.put("/detailram/:id", updateDetailRAM);
-simulationRoutes.delete("/detailram/:id", deleteDetailRAM);
-
-// Detail_CPU routes
-simulationRoutes.get("/detailcpu", getAllDetailCPU);
-simulationRoutes.get("/detailcpu/:id", getDetailCPUById);
-simulationRoutes.post("/detailcpu", createDetailCPU);
-simulationRoutes.put("/detailcpu/:id", updateDetailCPU);
-simulationRoutes.delete("/detailcpu/:id", deleteDetailCPU);
-
-// Detail_Motherboard routes
-simulationRoutes.get("/detailmotherboard", getAllDetailMotherboard);
-simulationRoutes.get("/detailmotherboard/:id", getDetailMotherboardById);
-simulationRoutes.post("/detailmotherboard", createDetailMotherboard);
-simulationRoutes.put("/detailmotherboard/:id", updateDetailMotherboard);
-simulationRoutes.delete("/detailmotherboard/:id", deleteDetailMotherboard);
 
 // Tipe_RAM routes
 simulationRoutes.get("/tiperam", getAllTipeRAM);
