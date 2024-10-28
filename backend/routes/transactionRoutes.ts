a5313a640e8505598e3def20527c1431c379e023
ibnu fadhil <fadhilgelaws@gmail.com>
2024-10-28 18:48:27 +0700

    better format name for routes

--- backend/routes/transactionRoutes.ts
+++ backend/routes/transactionRoutes.ts
@@ -27,35 +27,35 @@ import {
 
 const transaksiRoutes = express.Router();
 
-transaksiRoutes.get("/keranjang/:id_user", getAllCartByUserId);
-transaksiRoutes.get("/keranjang/detail/:id", clearCart);
-transaksiRoutes.post("/keranjang", addToCart);
-transaksiRoutes.put("/keranjang/:id", updateCartItem);
-transaksiRoutes.delete("/keranjang/:id", removeFromCart);
+transaksiRoutes.get("/cart/:id_user", getAllCartByUserId);
+transaksiRoutes.get("/cart/detail/:id", clearCart);
+transaksiRoutes.post("/cart", addToCart);
+transaksiRoutes.put("/cart/:id", updateCartItem);
+transaksiRoutes.delete("/cart/:id", removeFromCart);
 
-transaksiRoutes.get("/favorites/:id_user", getAllFavoritesByUserId);
-transaksiRoutes.post("/favorites", addToFavorites);
-transaksiRoutes.delete("/favorites/:id", removeFromFavorites);
+transaksiRoutes.get("/favorite/:id_user", getAllFavoritesByUserId);
+transaksiRoutes.post("/favorite", addToFavorites);
+transaksiRoutes.delete("/favorite/:id", removeFromFavorites);
 
 // Transaksi routes
-transaksiRoutes.get('/transaksi', getAllTransaction);
-transaksiRoutes.get('/transaksi/:id', getTransactionById);
-transaksiRoutes.post('/transaksi', createTransaction);
-transaksiRoutes.put('/transaksi/:id', updateTransaction);
-transaksiRoutes.delete('/transaksi/:id', deleteTransaction);
+transaksiRoutes.get('/transaction', getAllTransaction);
+transaksiRoutes.get('/transaction/:id', getTransactionById);
+transaksiRoutes.post('/transaction', createTransaction);
+transaksiRoutes.put('/transaction/:id', updateTransaction);
+transaksiRoutes.delete('/transaction/:id', deleteTransaction);
 
 // Detail_Transaksi routes
-transaksiRoutes.get('/detail-transaksi', getAllDetailTransaksi);
-transaksiRoutes.get('/detail-transaksi/:id', getDetailTransaksiById);
-transaksiRoutes.post('/detail-transaksi', createDetailTransaksi);
-transaksiRoutes.put('/detail-transaksi/:id', updateDetailTransaksi);
-transaksiRoutes.delete('/detail-transaksi/:id', deleteDetailTransaksi);
+transaksiRoutes.get('/transaction-detail', getAllDetailTransaksi);
+transaksiRoutes.get('/transaction-detail/:id', getDetailTransaksiById);
+transaksiRoutes.post('/transaction-detail', createDetailTransaksi);
+transaksiRoutes.put('/transaction-detail/:id', updateDetailTransaksi);
+transaksiRoutes.delete('/transaction-detail/:id', deleteDetailTransaksi);
 
 // Log_Transaksi routes
-transaksiRoutes.get('/log-transaksi', getAllLogTransaksi);
-transaksiRoutes.get('/log-transaksi/:id', getLogTransaksiById);
-transaksiRoutes.post('/log-transaksi', createLogTransaksi);
-transaksiRoutes.put('/log-transaksi/:id', updateLogTransaksi);
-transaksiRoutes.delete('/log-transaksi/:id', deleteLogTransaksi);
+transaksiRoutes.get('/transaction-log', getAllLogTransaksi);
+transaksiRoutes.get('/transaction-log/:id', getLogTransaksiById);
+transaksiRoutes.post('/transaction-log', createLogTransaksi);
+transaksiRoutes.put('/transaction-log/:id', updateLogTransaksi);
+transaksiRoutes.delete('/transaction-log/:id', deleteLogTransaksi);
 
 export default transaksiRoutes;
