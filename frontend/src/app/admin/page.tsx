86512ffe0069daefa43cd8f2e4cdeb0bea207874
Fathan <102466798+mhmdfathan@users.noreply.github.com>
2024-11-26 21:44:59 +0700

    feat: charts

---
+++ frontend/src/app/admin/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Page() {
+    return (
+        <div>
+            <h1>Admin Page</h1>
+            <p>Admin page content</p>
+        </div>
+    );
+}
