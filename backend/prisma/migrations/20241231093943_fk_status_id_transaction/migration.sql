c2f34572d1d62f1c51eed33d24d42ab3a2f55ae4
aditakbars <aditakbars33@gmail.com>
2025-01-07 16:27:08 +0700

    migrating my localdb

--- backend/prisma/migrations/20241231093943_fk_status_id_transaction/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE `transaction` ADD COLUMN `status_id` INTEGER NOT NULL DEFAULT 1;
-
--- AddForeignKey
-ALTER TABLE `transaction` ADD CONSTRAINT `transaction_status_id_fkey` FOREIGN KEY (`status_id`) REFERENCES `transaction_status`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
