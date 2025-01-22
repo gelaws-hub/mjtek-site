4a5cb4337c328f39074cba63c188df4f92c7723d
ibnu fadhil <fadhilgelaws@gmail.com>
2025-01-22 14:51:26 +0700

    fix some deployment issue : 1. downgrade reactpdf to 3.4.5 2. wrapping components with the useSearchParams with react Suspense

--- frontend/src/app/admin/layout.tsx
+++ frontend/src/app/admin/layout.tsx
@@ -1,19 +1,17 @@
-'use client';
+"use client";
 
 import Breadcrumb from "@/components/BreadCrump"; // Import Breadcrumb
 import { Suspense } from "react";
 
-export default function RootLayout({
+export default function AdminLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <Suspense>
-      <div className="mx-auto mt-6 w-[90%] md:w-[80%] lg:w-[75%]">
-        <Breadcrumb />
-        {children}
-      </div>
+      <Breadcrumb />
+      {children}
     </Suspense>
   );
 }
