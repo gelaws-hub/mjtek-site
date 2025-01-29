c072bd27a01cd34133d6b2b643c050ad20b9e3f5
ibnu fadhil <fadhilgelaws@gmail.com>
2025-01-29 22:47:07 +0700

    fix some configuration

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -33,13 +33,6 @@ const nextConfig = {
     eslint: {
         ignoreDuringBuilds: true,
     },
-    typescript: {
-      // !! WARN !!
-      // Dangerously allow production builds to successfully complete even if
-      // your project has type errors.
-      // !! WARN !!
-      ignoreBuildErrors: true,
-    },
   };
 
 export default nextConfig;
