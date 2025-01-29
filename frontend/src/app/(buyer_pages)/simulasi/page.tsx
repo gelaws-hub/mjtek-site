1613f42ae4818961938fb990cc0d06c58e112b36
ibnu fadhil <fadhilgelaws@gmail.com>
2025-01-29 22:48:26 +0700

    reorganize simulation route frontend structure

--- frontend/src/app/(buyer_pages)/simulasi/page.tsx
+++
@@ -1,9 +0,0 @@
-import MainComponentSim from "./components/MainComponentSim";
-export default function Simulasi() {
-  return (
-    <div className="container mx-auto py-4 min-h-screen">
-      <MainComponentSim />
-      {/* <StorageComponentSim /> */}
-    </div>
-  );
-}
