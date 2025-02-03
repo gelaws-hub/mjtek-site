acbd607fc937ec5c3806249d5e1d9212861d1308
ibnu fadhil <fadhilgelaws@gmail.com>
2025-02-03 14:45:47 +0700

    fix register and activation token

--- backend/auth/userController.ts
+++ backend/auth/userController.ts
@@ -109,12 +109,20 @@ export const registerUser = async (req: Request, res: Response) => {
         phone_number,
         activation_token: activationToken,
         reset_password_token: null,
-        is_active: true,
+        is_active: false,
         role_name: "buyer", // Set default role sebagai user biasa(pembeli)
         profile_pic:
           "/image.png",
       },
     });
+
+    const userCreated = {
+      name: user.name,
+      email: user.email,
+      address: user.address,
+      phone_number: user.phone_number,
+      role_name: user.role_name,
+    }
 
     // Kirim email aktivasi
     const activationLink = `${getOriginFromRequest(req)}/activate?token=${activationToken}`;
@@ -122,8 +130,8 @@ export const registerUser = async (req: Request, res: Response) => {
     await sendActivationEmail(email, activationLink);
 
     res.status(201).json({
-      message: 'User registered successfully. Please check your email to activate your account.',
-      user,
+      message: 'User registered successfully',
+      userCreated,
     });
   } catch (error) {
     console.error(error);
